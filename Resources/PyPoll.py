5ad97a4a5ef70d43809dfa715483f62ed2941290
Alaina Sliwinski <asliwinski23@gmail.com>
2021-03-03 20:53:18 -0500

    Adding PyPoll.py file.

---
+++ Resources/PyPoll.py
@@ -0,0 +1,26 @@
+# Import pandas
+import pandas as pd
+import os
+
+# Create a filename variable to a direct or indirect path to the file.
+file_to_save = open(r"C:\Users\candy\OneDrive\Documents\Bootcamp\Module 3\Election Analysis\Analysis\election_analysis.txt", "w")
+# Write inside file
+file_to_save.write("Counties in the Election\n-------------------------\nArapahoe\nDenver\nJefferson")
+# Close the file
+file_to_save.close()  
+
+
+# Read polling csv
+df_polling_results = pd.read_csv("election_results.csv")
+print(df_polling_results.head())
+
+# Analysis to run:
+# Total number of votes cast
+
+# A complete list of candidates who received votes
+
+# Total number of votes each candidate received
+
+# Percentage of votes each candidate won
+
+# The winner of the election based on popular vote
