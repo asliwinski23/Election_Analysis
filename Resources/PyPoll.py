569fead8b6a9f9e1220815394965ff96dbbb260d
Alaina Sliwinski <asliwinski23@gmail.com>
2021-03-06 15:22:46 -0500

    added challenge analysis

--- Resources/PyPoll.py
+++ Resources/PyPoll.py
@@ -2,33 +2,51 @@
 import pandas as pd
 import os
 
-# Create a filename variable to a direct or indirect path to the file.
-file_to_save = open(r"C:\Users\candy\OneDrive\Documents\Bootcamp\Module 3\Election Analysis\Election_Analysis\Analysis\election_analysis.txt", "w")
-# Write inside file
-file_to_save.write("Counties in the Election\n-------------------------\nArapahoe\nDenver\nJefferson")
-# Close the file
-file_to_save.close()  
-
-
 # Read polling csv
 df_polling_results = pd.read_csv("election_results.csv")
-# print(df_polling_results.head())
 
 # Analysis to run:
+
 # Total number of votes cast
 total_votes = 0
 
 for ID in df_polling_results["Ballot ID"]:
     total_votes += 1
 
-# print(total_votes)
+# Make complete list of counties that voted in the election
+voting_counties = []
 
-# Wrote total number of votes to text file
+# Make dictionary to match county with number of votes cast
+votes_by_county = {}
 
-# A complete list of candidates who received votes
+for county in df_polling_results["County"]:
+    if county not in voting_counties:
+        voting_counties.append(county)
+        votes_by_county[county] = 0
+
+    votes_by_county[county] += 1
+
+# Determine the county that submitted the most votes
+# Set variables
+county_most_votes = ""
+count_county_most_votes = 0
+percentage_county_most_votes = 0
+
+# Find percentage of total votes that each county had
+for county in votes_by_county:
+    county_votes = votes_by_county[county]
+    percentage_county_votes = (county_votes / total_votes) * 100
+
+     # Find the county that submitted the most votes
+    if votes_by_county[county] > count_county_most_votes and percentage_county_votes > percentage_county_most_votes:
+        county_most_votes = county
+        count_county_most_votes = votes_by_county[county]
+        percentage_county_most_votes = percentage_county_votes
+
+# Make a list of candidates
 candidate_options = []
 
-#Total number of votes each candidate received
+# Make a dictionary pairing each candidate to the number of votes they received
 candidate_votes = {}
 
 for name in df_polling_results["Candidate"]:
@@ -38,53 +56,107 @@ for name in df_polling_results["Candidate"]:
         candidate_votes[name] = 0
     candidate_votes[name] += 1
 
-# print(candidate_options)
-print(candidate_votes)
 
-  # Percentage of votes each candidate won
+# Find percentage of votes each candidate received in order to determine the election winner 
+# Set variables   
 winning_candidate = ""
 winning_count = 0
 winning_percentage = 0
 
+# Find percentage votes
 for name in candidate_votes:
     votes = candidate_votes[name]
     vote_percentage = (votes / total_votes) * 100
-    # print(f"{name}: received {vote_percentage:.2f}% of the vote.")
 
-# The winner of the election based on popular vote
+    # Determine the winner of the election based on popular vote
     if candidate_votes[name] > winning_count and vote_percentage > winning_percentage:
         winning_count = candidate_votes[name]
         winning_percentage = vote_percentage
         winning_candidate = name
-    
-    # print(f"{name}: {vote_percentage:.1f}% ({votes:,})\n")
 
-election_summary = (
+# Print layouts for terminal and text file where you will write election results
+election_summary_for_txtfile = (
+    f"Election Results\n"
+    f"-------------------------\n"
+    f"Total Votes: {total_votes:,}\n"
+    f"-------------------------\n\n")
+
+election_summary_for_print = (
     f"Election Results\n"
     f"-------------------------\n"
     f"Total Votes: {total_votes:,}\n"
     f"-------------------------\n")
 
-winning_candidate_summary = (
+winning_candidate_summary_for_txtfile = (
+    "\n"
     f"-------------------------\n"
     f"Winner: {winning_candidate}\n"
     f"Winning Vote Count: {winning_count:,}\n"
     f"Winning Percentage: {winning_percentage:.1f}%\n"
-    f"-------------------------\n")
+    f"-------------------------")
 
-# print(election_summary)
-# print(winning_candidate_summary)
-# with open(file_to_save, "w") as txt_file:
-#     file_to_save.write(winning_candidate_summary)
-# Create a filename variable to a direct or indirect path to the file.
+winning_candidate_summary_for_print = (
+    f"-------------------------\n"
+    f"Winner: {winning_candidate}\n"
+    f"Winning Vote Count: {winning_count:,}\n"
+    f"Winning Percentage: {winning_percentage:.1f}%\n"
+    f"-------------------------")
+
+# PRINT ELECTION RESULTS TO TERMINAL
+# general summary
+print(election_summary_for_print)
+
+# election breakdown by county
+for county in votes_by_county:
+    county_votes = votes_by_county[county]
+    percentage_county_votes = (county_votes / total_votes) * 100
+    county_results = f"{county}: {percentage_county_votes:.1f}% ({county_votes:,})\n"
+    print(county_results)
+
+print(
+    f"-------------------------\n"
+    f"Largest County Turnout: {county_most_votes}\n"
+    f"-------------------------\n"
+    )
+
+# election breakdown by candidate
+for name in candidate_votes:
+    votes = candidate_votes[name]
+    vote_percentage = (votes / total_votes) * 100
+    candidate_results = (f"{name}: {vote_percentage:.1f}% ({votes:,})\n")
+    print(candidate_results)
+
+print(winning_candidate_summary_for_print)
+
+# TRANSFER ELECTION RESULTS TO TEXT FILE
+# Set the file you are going to write on
 file_to_save = open(r"C:\Users\candy\OneDrive\Documents\Bootcamp\Module 3\Election Analysis\Election_Analysis\Analysis\election_analysis.txt", "w")
 # Write inside file
-file_to_save.write(election_summary)
+# general election results
+file_to_save.write(election_summary_for_txtfile)
+
+# election breakdown by county
+for county in votes_by_county:
+    county_votes = votes_by_county[county]
+    percentage_county_votes = (county_votes / total_votes) * 100
+    county_results = f"{county}: {percentage_county_votes:.1f}% ({county_votes:,})\n"
+    file_to_save.write(county_results)
+
+file_to_save.write(
+    "\n"
+    f"-------------------------\n"
+    f"Largest County Turnout: {county_most_votes}\n"
+    f"-------------------------\n\n"
+    )
+
+
+# election breakdown by candidate
 for name in candidate_votes:
     votes = candidate_votes[name]
     vote_percentage = (votes / total_votes) * 100
     candidate_results = (f"{name}: {vote_percentage:.1f}% ({votes:,})\n")
     file_to_save.write(candidate_results)
-file_to_save.write(winning_candidate_summary)
+
+file_to_save.write(winning_candidate_summary_for_txtfile)
 # Close the file
 file_to_save.close()  
